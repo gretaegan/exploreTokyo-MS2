10e3e7ee791afe5a716fc51dabd1912c2c09034f
gretaegan <greta.egan@gmail.com>
2021-03-05 16:09:49 +0000

    Add locations variable to map.js

---
+++ assets/js/map.js
@@ -0,0 +1,17 @@
+
+var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+const locations = {
+    shinjuku: {
+        lat: 35.70724705599633,
+        lng: 139.7120326497082
+    },
+    shibuya: {
+        lat: 35.6693138677115,
+        lng: 139.69646301791374
+    },
+    asakusa: {
+        lat: 35.71537424874052,
+        lng: 139.79714056750478
+    }
+}
