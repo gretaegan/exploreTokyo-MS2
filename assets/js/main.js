f4c8d6bd3b02be6715195e5b55673827c9841aac
gretaegan <greta.egan@gmail.com>
2021-03-09 14:21:35 +0000

    Add JQuery interactivity to highlights.html page

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+//Code to toggle the highlights information //
+
+$(document).ready(function(){
+    $('#btn-1').on('click' , function(){
+        $('#highlight-1').toggle();
+    })
+
+    $('#btn-2').on('click' , function(){
+        $('#highlight-2').toggle();
+    })
+
+    $('#btn-3').on('click' , function(){
+        $('#highlight-3').toggle();
+    })
+
+    $('#btn-4').on('click' , function(){
+        $('#highlight-4').toggle();
+    })
+
+    $('#btn-5').on('click' , function(){
+        $('#highlight-5').toggle();
+    })
+
+    $('#btn-6').on('click' , function(){
+        $('#highlight-6').toggle();
+    })
+
+});
--- assets/js/map.js
+++ assets/js/map.js
@@ -1,22 +1,5 @@
 
-
-const locations = {
-    shinjuku: {
-        lat: 35.70724705599633,
-        lng: 139.7120326497082
-    },
-    shibuya: {
-        lat: 35.6693138677115,
-        lng: 139.69646301791374
-    },
-    asakusa: {
-        lat: 35.71537424874052,
-        lng: 139.79714056750478
-    }
-}
-
-// Code to initialize the mao //
-
+// Code to initialize the map //
 
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
@@ -29,8 +12,6 @@ function initMap() {
 }
 
 // Code to display map and show area sections //
-
-
 $(document).ready(function(){
     $('#shinjukupic').on('click', function(){
         $('#shinjuku-info').show();
@@ -48,14 +29,26 @@ $(document).ready(function(){
 
 
 
+// Change center of map (found at https://stackoverflow.com/questions/28499141/how-to-change-google-map-center-by-clicking-a-button) and
+// code to clear markers// 
 
-// Change center of map - https://stackoverflow.com/questions/28499141/how-to-change-google-map-center-by-clicking-a-button// 
 function newLocation(newLat, newLng) {
     map.setCenter({
         lat: newLat,
         lng: newLng,
     });
 }
+
+function clearMarkers() {
+    for (let i = 0; i < markers.length; i++) {
+        if (markers[i]) {
+            markers[i].setMap(null);
+        }
+    }
+    markers = [];
+}
+
+
 
 $(document).ready(function(){
 
@@ -71,4 +64,29 @@ $(document).ready(function(){
         newLocations(35.71537424874052, 139.79714056750478);
     });
 })
+
+
+// Add markers to the map // 
+
+var markers = [];
+
+
+const locations = {
+
+    shinjuku : {
+        latitude : "35.701392431518805",
+        longitude: "139.70842776085"
+    },
+
+    shibuya : {
+        latitude: "35.66805874481764",
+        longitude: "139.69569054172985"
+    },
+
+    asakusa : {
+        latitude: "35.718370786924936",
+        longitude: "139.79589602254183"
+    }
+}
+
 
