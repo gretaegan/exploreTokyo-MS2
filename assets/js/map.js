f2e3af5273861674166cda01cffb17a238d7ef07
gretaegan <greta.egan@gmail.com>
2021-03-09 10:50:25 +0000

    Add area sections to explore page and Javascript to change center of the map to map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,5 +1,5 @@
 
-var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var labels = [];
 
 const locations = {
     shinjuku: {
@@ -15,3 +15,29 @@ const locations = {
         lng: 139.79714056750478
     }
 }
+
+// Change center of map - https://stackoverflow.com/questions/28499141/how-to-change-google-map-center-by-clicking-a-button// 
+
+
+function newLocation(newLat, newLng) {
+    map.setCenter({
+        lat: newLat,
+        lng: newLng,
+    });
+}
+
+$(document).ready(function(){
+
+    $('#shinjuku-info').on('click' , function(){
+        newLocation(35.70724705599633, 139.7120326497082);
+    });
+
+    $('#shibuya-info').on('click' , function(){
+        newLocation(35.6693138677115, 139.69646301791374);
+    });
+
+    $('#asakusa-info').on('click' , function(){
+        newLocations(35.71537424874052, 139.79714056750478);
+    });
+})
+
