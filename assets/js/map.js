a28779688032d455ba360705cba7ebfa292ed088
gretaegan <greta.egan@gmail.com>
2021-03-10 12:15:17 +0000

    Rewrite code to initiate Googlemap

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,92 +1,31 @@
+//Initiate the Map// 
 
-// Code to initialize the map //
+let map;
 
 function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 10,
+    map = new.google.maps.Map(document.getElementById("map"), {
         center: {
-            lat: 35.69883763050049,
-            lng: 139.76283314117453
-        }
+            lat: 35.67876072537644,
+            lng: 139.76763965965216
+        },
+        zoom: 10
     });
 }
-
-// Code to display map and show area sections //
-$(document).ready(function(){
-    $('#shinjukupic').on('click', function(){
-        $('#shinjuku-info').show();
-    })
-
-    $('#shibuyapic').on('click', function(){
-        $('#shibuya-info').show();
-    })
-
-    $('#asakusapic').on('click', function(){
-        $('#asakusa-info').show();
-    })
-});
-
-
-
-
-// Change center of map (found at https://stackoverflow.com/questions/28499141/how-to-change-google-map-center-by-clicking-a-button) and
-// code to clear markers// 
-
-function newLocation(newLat, newLng) {
-    map.setCenter({
-        lat: newLat,
-        lng: newLng,
-    });
-}
-
-function clearMarkers() {
-    for (let i = 0; i < markers.length; i++) {
-        if (markers[i]) {
-            markers[i].setMap(null);
-        }
-    }
-    markers = [];
-}
-
-
-
-$(document).ready(function(){
-
-    $('#shinjukupic').on('click' , function(){
-        newLocation(35.70724705599633, 139.7120326497082);
-    });
-
-    $('#shibuyapic').on('click' , function(){
-        newLocation(35.6693138677115, 139.69646301791374);
-    });
-
-    $('#asakusapic').on('click' , function(){
-        newLocations(35.71537424874052, 139.79714056750478);
-    });
-})
-
-
-// Add markers to the map // 
-
-var markers = [];
-
 
 const locations = {
-
-    shinjuku : {
-        latitude : "35.701392431518805",
-        longitude: "139.70842776085"
+    shinjuku: {
+        lat: 35.70160153265351,
+        lng:  139.70791277672734
     },
 
-    shibuya : {
-        latitude: "35.66805874481764",
-        longitude: "139.69569054172985"
+    shibuya: {
+        lat: 35.667221985258365,
+        lng: 139.69491806554595
     },
 
-    asakusa : {
-        latitude: "35.718370786924936",
-        longitude: "139.79589602254183"
+    asakusa: {
+        lat: 35.718318522689614,
+        lng: 139.79606768391602
     }
 }
-
 
