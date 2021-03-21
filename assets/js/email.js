2f61e2ec77b9c40f3a998dce2e85366ca157e601
gretaegan <greta.egan@gmail.com>
2021-03-21 10:08:11 +0000

    Add changes to JS code for contact form

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,18 +1,18 @@
 
 
 function sendMail(contactForm){
-    emailjs.send("service_616bzzc","tokyo", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "info_request": contactForm.inforequest.value
+    emailjs.send("gmail","tokyo", {
+        from_name: contactForm.name.value,
+        from_email: contactForm.emailaddress.value,
+        info_request: contactForm.inforequest.value
     })
     .then (
         function(response){
-            console.log("SUCCESS!", response);
+            alert("SUCCESS!", response);
         },
         function(error){
-            console.log("Failed", error);
+            alert("Something went wrong..please try again!", error);
         }
-    )
+    );
 }
 
