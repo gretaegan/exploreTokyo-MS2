98e4b25c13c9f3976f79ee8f0c99850ae60df51a
gretaegan <greta.egan@gmail.com>
2021-03-22 12:49:27 +0000

    Change JQuery functions to slidedown

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,29 @@
 //Code to toggle the highlights information //
 
 $(document).ready(function(){
-    $('#btn-1').hover(function(){
-        $('#highlight-1').show(1000);
+
+    $('#btn-1').click(function(){
+        $('#highlight-1').slideDown(5000);
     })
 
-    $('#btn-2').hover(function(){
-        $('#highlight-2').show(1000);
+    $('#btn-2').click(function(){
+        $('#highlight-2').slideDown(5000);
     })
 
-    $('#btn-3').hover(function(){
-        $('#highlight-3').show(1000);
+    $('#btn-3').click(function(){
+        $('#highlight-3').slideDown(5000);
     })
 
-    $('#btn-4').hover(function(){
-        $('#highlight-4').show(1000);
+    $('#btn-4').click(function(){
+        $('#highlight-4').slideDown(5000);
     })
 
-    $('#btn-5').hover(function(){
-        $('#highlight-5').show(1000);
+    $('#btn-5').click(function(){
+        $('#highlight-5').slideDown(5000);
     })
 
-    $('#btn-6').hover(function(){
-        $('#highlight-6').show(1000);
+    $('#btn-6').click(function(){
+        $('#highlight-6').slideDown(5000);
     })
 
 });
