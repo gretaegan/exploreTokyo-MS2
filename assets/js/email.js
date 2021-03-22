eba67f66757a171181f69f732134c40c2d80e185
gretaegan <greta.egan@gmail.com>
2021-03-22 16:40:10 +0000

    Fix errors in JS Code and html for contact form

--- assets/js/email.js
+++ assets/js/email.js
@@ -2,16 +2,16 @@
 //Code to make the contact form interactive - sourced at https://www.emailjs.com/docs/tutorial/creating-contact-form///
 
 
-window.onload = function() {
-            document.getElementById('contactForm').addEventListener('submit', function(event) {
+
+            document.getElementById("contactForm").addEventListener("submit", function(event) {
                 event.preventDefault();
-                emailjs.sendForm('service_jqwbfeg', 'gmail-tokyo', this)
+                console.log(this);
+                emailjs.sendForm("service_jqwbfeg","gmail-tokyo", this)
                     .then(function() {
-                        alert('SUCCESS!.. a member of our team will get back to you shortly');
+                        alert("SUCCESS!.. a member of our team will get back to you shortly");
                     }, function(error) {
-                        alert('Something went wrong... please try again', error);
+                        alert("Something went wrong... please try again", error);
                     });
             });
-        }
 
 
