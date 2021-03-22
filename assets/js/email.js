0e6e90143a726b43919ef5c91644a19ad441d412
gretaegan <greta.egan@gmail.com>
2021-03-22 14:05:27 +0000

    Fix code for contact form including JS to make form interactive

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,20 +1,17 @@
 
+//Code to make the contact form interactive - sourced at https://www.emailjs.com/docs/tutorial/creating-contact-form///
 
-function sendMail(contactForm){
-    emailjs.send("gmail","tokyo", {
-        from_name: contactForm.name.value,
-        from_email: contactForm.emailaddress.value,
-        info_request: contactForm.inforequest.value
-    })
-    .then (
-        function(response){
-            alert("SUCCESS!", response);
-        },
-        function(error){
-            alert("Something went wrong..please try again!", error);
+
+window.onload = function() {
+            document.getElementById('contactForm').addEventListener('submit', function(event) {
+                event.preventDefault();
+                emailjs.sendForm('service_jqwbfeg', 'gmail-tokyo', this)
+                    .then(function() {
+                        alert('SUCCESS!.. a member of our team will get back to you shortly');
+                    }, function(error) {
+                        alert('Something went wrong... please try again', error);
+                    });
+            });
         }
-    );
-}
-
 
 
